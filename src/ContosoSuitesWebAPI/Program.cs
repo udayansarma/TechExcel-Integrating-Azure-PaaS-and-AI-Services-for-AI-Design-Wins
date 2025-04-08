13555f58e112f9510472c8cee68d8b336b516da1
Udy Sarma <udsarm@microsoft.com>
2025-04-08 14:44:46 -0700

    second push

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -76,25 +76,53 @@ app.MapGet("/", async () =>
     .WithOpenApi();
 
 // Retrieve the set of hotels from the database.
+/*
 app.MapGet("/Hotels", async () => 
 {
     throw new NotImplementedException();
 })
     .WithName("GetHotels")
     .WithOpenApi();
+    */
+app.MapGet("/Hotels", async () => 
+{
+    var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
+    return hotels;
+})
+    .WithName("GetHotels")
+    .WithOpenApi();
 
 // Retrieve the bookings for a specific hotel.
+/*
 app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
 {
     throw new NotImplementedException();
 })
     .WithName("GetBookingsForHotel")
     .WithOpenApi();
+    */
+app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+{
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
+    return bookings;
+})
+    .WithName("GetBookingsForHotel")
+    .WithOpenApi();
+
 
 // Retrieve the bookings for a specific hotel that are after a specified date.
+/*
 app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
 {
     throw new NotImplementedException();
+})
+    .WithName("GetRecentBookingsForHotel")
+    .WithOpenApi();
+*/
+app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+{
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
+    return bookings;
 })
     .WithName("GetRecentBookingsForHotel")
     .WithOpenApi();
--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-o25e4hcbfoo3s FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-o25e4hcbfoo3s;
+ALTER ROLE db_datawriter ADD MEMBER id-o25e4hcbfoo3s;
 
